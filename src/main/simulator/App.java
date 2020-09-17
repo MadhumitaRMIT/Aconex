8ac4a1105b474042de59bcdcee1cec172c27e60d
Madhu <mitha.rajkumar@gmail.com>
2020-09-17 23:17:34 +1000

    Adding Input file

--- src/main/simulator/App.java
+++ src/main/simulator/App.java
@@ -2,6 +2,6 @@ package main.simulator;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello,");
+        System.out.println("Hello, world");
     }
 }
